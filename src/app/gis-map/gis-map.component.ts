75b56a51a211338b7c691b08f3d28aa7d57bdd2c
happyDebugging <87609041+happyDebugging@users.noreply.github.com>
2024-08-08 23:10:29 +0200

    Customized marker popup buttons.

--- src/app/gis-map/gis-map.component.ts
+++ src/app/gis-map/gis-map.component.ts
@@ -155,6 +155,10 @@ export class GisMapComponent implements OnInit, AfterViewInit {
         popupInfo = '<b>' + marker.Address + '</b><br>' + marker.StateDescription + '  ' +
           `<div class="d-grid">
             <button type="button" class="btn btn-secondary btn-sm edit"> 
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+              </svg>
               Edit
             </buton>
             <button type="button" class="btn btn-primary btn-sm navigateToHere"> 
@@ -162,6 +166,7 @@ export class GisMapComponent implements OnInit, AfterViewInit {
               <path d="M11 8.5A2.5 2.5 0 0 0 8.5 6H7V4.534a.25.25 0 0 0-.41-.192L4.23 6.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 7 8.466V7h1.5A1.5 1.5 0 0 1 10 8.5V11h1z"/>
               <path fill-rule="evenodd" d="M6.95.435c.58-.58 1.52-.58 2.1 0l6.515 6.516c.58.58.58 1.519 0 2.098L9.05 15.565c-.58.58-1.519.58-2.098 0L.435 9.05a1.48 1.48 0 0 1 0-2.098zm1.4.7a.495.495 0 0 0-.7 0L1.134 7.65a.495.495 0 0 0 0 .7l6.516 6.516a.495.495 0 0 0 .7 0l6.516-6.516a.495.495 0 0 0 0-.7L8.35 1.134Z"/>
               </svg>
+              Πλοήγηση
             </buton>
           </div>
       `;
@@ -193,6 +198,7 @@ export class GisMapComponent implements OnInit, AfterViewInit {
               <path d="M11 8.5A2.5 2.5 0 0 0 8.5 6H7V4.534a.25.25 0 0 0-.41-.192L4.23 6.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 7 8.466V7h1.5A1.5 1.5 0 0 1 10 8.5V11h1z"/>
               <path fill-rule="evenodd" d="M6.95.435c.58-.58 1.52-.58 2.1 0l6.515 6.516c.58.58.58 1.519 0 2.098L9.05 15.565c-.58.58-1.519.58-2.098 0L.435 9.05a1.48 1.48 0 0 1 0-2.098zm1.4.7a.495.495 0 0 0-.7 0L1.134 7.65a.495.495 0 0 0 0 .7l6.516 6.516a.495.495 0 0 0 .7 0l6.516-6.516a.495.495 0 0 0 0-.7L8.35 1.134Z"/>
               </svg>
+              Πλοήγηση
             </buton>
           </div>
       `;
@@ -213,9 +219,8 @@ export class GisMapComponent implements OnInit, AfterViewInit {
       }
 
       this.fireHydrantLayerGroup = L.layerGroup(this.fireHydrantLayer);
-
     }
-    //const editPointButton = L.DomUtil.get('button-submit');
+    
   }
 
   GetUserLocation() {
