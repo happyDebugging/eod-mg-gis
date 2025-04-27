a4c87e11c3b093d543307f3302d74b42315376bf
happyDebugging <87609041+happyDebugging@users.noreply.github.com>
2025-04-27 22:10:09 +0200

    Test environments.

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { createClient, SupabaseClient, User } from '@supabase/supabase-js';
-import { environment } from '../../environments/environment.development';
-//import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment';
 import { DbFunctionService } from '../shared/services/db-functions.service';
 import { Router } from '@angular/router';
 
